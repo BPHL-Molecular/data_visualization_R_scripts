cb4ec9cdb2f54ebd18bfb0825328faec88afa1f5
thsalikiprasanna <59940227+thsalikiprasanna@users.noreply.github.com>
2022-10-03 08:42:53 -0400

    Update ggtree script.R

--- ggtree script.R
+++ ggtree script.R
@@ -13,25 +13,31 @@ get.tree(tree)
 get.fields(tree) #returns vector of attributes stored in the object and associated with phylogeny
 get.data(tree) #returns a tibble of all the associated data
 SNPs_boot.treefile <- system.file("extdata", "sample.nwk", package="treeio")
+
 tree <- read.tree(SNPs_boot.treefile)
+tree2 <- groupClade(tree, c(26,24))
 ggtree(tree2) +
   geom_point(aes(shape=isTip, color=isTip, size=3)) +
   geom_tiplab(size=3, color="purple")
 ggtree(tree2) + 
   theme_tree2() +
   geom_tiplab(size=4, color="orange", linesize=0.02, align=TRUE)
+
 # to add the internal node numbers
 ggtree(tree2) + geom_text(aes(label=node), hjust=-.3) 
 ggtree(tree2) + 
   geom_tiplab(size=3, color="purple")
+
 # labelling clades
 ggtree(tree2) + 
   geom_cladelabel(node=31, label="Some random clade", color="red")
+
 # add offset to adjust the position of the label
 ggtree(tree2) + 
   geom_tiplab() + 
   geom_cladelabel(node=31, label="Some random clade", 
                   color="red2", offset=.1)
+
 # to add another label for the clade connecting L and J
 ggtree(tree2) + 
   geom_tiplab(size=4, color="purple") + 
@@ -39,11 +45,13 @@ ggtree(tree2) +
                   color="red", offset=.1) + 
   geom_cladelabel(node=22, label="A different clade", 
                   color="green", offset=.1)
+
 #To highlight the entire clade
 ggtree(tree2) + 
   geom_tiplab(size=3, color="purple") + 
   geom_hilight(node=31, fill="gold") + 
   geom_hilight(node=22, fill="purple", type="rect")
+
 # connecting taxa - to show evolutionary events
 ggtree(tree2) + 
   geom_tiplab() + 
